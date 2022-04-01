d22cfc8631f3e948fba4a00c275055c6c0a953f7
Stephen Huang <stephen_1996@live.ca>
2022-04-01 08:26:29 +0000

    added alert and escape function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,102 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
 
+$(document).ready(function() {
+  console.log('jQuery Loaded');
+
+  //Escaping text function to prevent XSS attack
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  //hide error box when page is loaded
+  $('.error').hide();
+
+  //creates the tweet content for container body
+
+  const createTweetElement = function(tweet) {
+  let $tweet = $('<article>').addClass('tweets-container');
+  let charObj =`
+  <header>
+        <div class="tweetmain">
+          <img src=${escape(tweet.user.avatars)} width=50px height=50px>
+          <p>${escape(tweet.user.name)}</p>
+        </div>
+        <p class="handle">${escape(tweet.user.handle)}</p>
+      </header>
+      <p class="tweet">${escape(tweet.content.text)}</p>
+      <footer>
+        <p>${escape(timeago.format(tweet.created_at))}</p>
+        <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+  `;
+  let tweets = $tweet.append(charObj);
+  return tweets;
+};
+
+
+  // responsible for taking in an array of tweet objects and appending to tweets-container
+
+  const renderTweets = function (tweets) {
+    for (let content of tweets) {
+      console.log(tweets);
+      let newTweet = createTweetElement(content);
+      let $tweetscontainer = $('.maintweetcontainer');
+      $tweetscontainer.append(newTweet);
+    } 
+  }
+
+
+  //"GETS" tweet from /tweets URL and loads it onto the page
+
+  const loadTweet = function() {
+    $.ajax({
+      url:"/tweets",
+      type:"GET"
+    }).then ((data) => {
+      renderTweets(data);
+    })
+  }
+
+  loadTweet();
+
+
+  //Prohibits submit button from default behaviour
+
+  $('#tweets').on('submit', function(e) {
+    e.preventDefault();
+    const preTweet = $('#tweets').serialize();
+    const finalTweet = $('#tweet-text').val();
+    $('#alert').slideUp();
+
+  //Slide down if tweet length is more than 140 or empty
+    if (finalTweet.length > 140 || finalTweet === '') {
+      return $('#alert').slideDown();
+    }
+  
+  //Slide up when fields are satisfied
+
+    $.ajax ({
+      type: 'POST',
+      url: '/tweets',
+      data: preTweet
+    }).then((data) => {
+      loadTweet();
+      $('#tweet-text').val('');
+      $('.counter').val(140);
+    })
+
+  })
+
+});
+
+//xhr works on network but does not print into body
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,20 @@
-jquery === $;
-
 $(document).ready(function() {
-  console.log('Ready');
+  console.log('jQuery Loaded');
   
   // counts characters in textarea
-  
+  $('#tweet-text').on('keyup', function(event) {
+    const length = 140;
+    let message = event.target.value;
+    let input = message.length;
+    let remainingChar = length - input;
+
+    if (input > length) {
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "black")
+    }
+
+    $('.counter').html(remainingChar);
+  })
   
 });
